71d7e0977ac19d8674a4bcf62e2797112ce43cd7
Kartik Pawar <kartikpawar1290@gmail.com>
2025-04-17 00:14:31 +0530

    new file  added

---
+++ DSA+Java/Switch_Nested_cases.java
@@ -0,0 +1,13 @@
+import java.util.*;
+
+public class main {
+    
+    public static main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        String fruit = in.next();
+
+        if (fruit.equals("mango")) {
+            System.out.println("King of friut");
+        }
+    }
+}
