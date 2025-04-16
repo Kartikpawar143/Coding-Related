c362b228cf604908f9a6bd861beddcd5ff73c588
Kartik Pawar <kartikpawar1290@gmail.com>
2025-04-16 23:56:58 +0530

    done

---
+++ DSA+Java/main.java
@@ -0,0 +1,12 @@
+import java.util.*;
+public class main {
+    public static void main(String[] args) {
+        int salary = 25000;
+        if (salary > 10000) {
+            salary = salary + 2000;
+        }else{
+            salary = salary +1000;
+        }
+    }
+}
+
