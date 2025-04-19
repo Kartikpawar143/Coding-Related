7aaef215ba952a058ad67535d1eb86f7e2ca9132
Kartik Pawar <kartikpawar1290@gmail.com>
2025-04-19 13:52:34 +0530

    New way to write the switch cases

--- DSA+Java/Switch.java
+++ DSA+Java/Switch.java
@@ -29,15 +29,20 @@ public class Switch {
         // }
 
         int day = sc.nextInt();
-        switch (day){
-        case 1 -> System.out.println("Monday");
-        case 2 -> System.out.println("Tuesday");
-        case 3 -> System.out.println("Wednesday");
-        case 4 -> System.out.println("Thursday");
-        case 5 -> System.out.println("Friday");
-        case 6 -> System.out.println("Saturday");
-        case 7 -> System.out.println("Sunday");
-        }
+        // switch (day){
+        // case 1 -> System.out.println("Monday");
+        // case 2 -> System.out.println("Tuesday");
+        // case 3 -> System.out.println("Wednesday");
+        // case 4 -> System.out.println("Thursday");
+        // case 5 -> System.out.println("Friday");
+        // case 6 -> System.out.println("Saturday");
+        // case 7 -> System.out.println("Sunday");
+        // }
          
+
+        switch (day) {
+            case 1, 2, 3, 4, 5 -> System.out.println("Weekday");
+            case 6, 7 -> System.out.println("Weekend");
+        }
     }
 }
