d2e52dbc967b501d74af4cc6c4dd241f3ce441d9
Kartik Pawar <kartikpawar1290@gmail.com>
2025-04-20 11:36:48 +0530

    function

---
+++ DSA+Java/Funtions_Methods.java
@@ -0,0 +1,11 @@
+import java.util.*;
+public class Funtions_Methods {
+    public static void main(String[] args) {
+        sum();
+    }
+    static void sum() {
+        Scanner sc = new Scanner (System.in);
+        System.out.println("Enter the first no.: ");
+        int a = sc.nextInt();
+    }
+}
