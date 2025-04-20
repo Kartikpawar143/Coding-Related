1a35ac501860c5afd6c9ac194adab4736caf924a
Kartik Pawar <kartikpawar1290@gmail.com>
2025-04-20 13:01:24 +0530

    Calculator using function

--- DSA+Java/Funtions_Methods.java
+++ DSA+Java/Funtions_Methods.java
@@ -1,16 +1,38 @@
 import java.util.*;
 public class Funtions_Methods {
     public static void main(String[] args) {
-        sum();
-    }
-    static void sum() {
-        Scanner sc = new Scanner (System.in);
-        System.out.print("Enter the first no.: ");
-        int a = sc.nextInt();
-        System.out.print("Enter the second no.: ");
-        int b = sc.nextInt();
-        int c = a + b;
-        System.out.print("Addition of two no. is: " + c);
+        //sum();
+        
+        int result = sum2();
+        System.out.println("The sum is: " + result);
 
     }
+    static int sum2(){
+        Scanner sc = new Scanner (System.in);
+        System.out.println("Enter the first no.: ");
+        int a = sc.nextInt();
+        System.out.println("Enter the second no.: ");
+        int b = sc.nextInt();
+        int c = a + b;
+        return c;
+    }
+
+
+
+    // static void sum() {
+    //     Scanner sc = new Scanner (System.in);
+    //     System.out.print("Enter the first no.: ");
+    //     int a = sc.nextInt();
+    //     System.out.print("Enter the second no.: ");
+    //     int b = sc.nextInt();
+    //     int c = a + b;
+    //     System.out.print("Addition of two no. is: " + c);
+
+    // }
+
+
 }
+
+
+
+
