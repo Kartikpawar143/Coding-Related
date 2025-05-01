833de57ac65a92483d7b0a095b714ece2fbc485f
Kartik Pawar <kartikpawar1290@gmail.com>
2025-05-01 12:56:27 +0530

    Banking Project

---
+++ DSA+Java/BankingProject.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+public class BankingProject {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println();
+        System.out.println("=========================================");
+        System.out.println();
+        System.out.println(" WELCOME TO THE AAPLI BANKING SYSTEM ");
+        System.out.println();
+        System.out.println("=========================================");
+        System.out.println();
+    }
+}
