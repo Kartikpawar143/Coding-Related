9b9715f020f63f87f2c6d78b86854add19b6e732
Kartik Pawar <kartikpawar1290@gmail.com>
2025-07-19 10:02:26 +0530

    done

---
+++ DSA+Java/Recursion7.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Recursion7{
+	public static void pintRev(String str, int idx){
+		System.out.println(str.charAt(idx));
+		printRev(str,)
+	}
+	public static void main(String args[]){
+
+	}
+}
